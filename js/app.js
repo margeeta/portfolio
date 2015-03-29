e46b6f0ff29f40c2ec89693ea144ed6e36653977
margeeta <margeeta@gmail.com>
2015-03-29 18:08:20 -0500

    more angular things

--- js/app.js
+++ js/app.js
@@ -2,18 +2,9 @@
 'use strict'
     var app = angular.module('portfolio', []);
 
-app.controller('portfolioCtrl', function($scope){
-    $scope.projects = [
-        {
-        name: '2Thrive',
-        image: '2thrive',
-        tag: 'tag • tag • tag',
-        description: 'string of text', },
-        {
-        name: 'Project Two',
-        image: '2thrive',
-        tag: 'tag',
-        description: 'string of text', },
-    ];
+
+app.controller('portfolioCtrl', function($scope, $http){
+    $httpBackend.get('/js/projects.json').success(function(data) {
+        $scope.projects = data;
 });
 })();
